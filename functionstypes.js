a437dce7d41aa726b7b7af2c6ef94df1d51905ec
Otavio Renan <fenixkc@hotmail.com>
2021-10-30 14:49:09 -0300

    tipos de funções

---
+++ functionstypes.js
@@ -0,0 +1,16 @@
+//realizar uma tarefa, e não devolve nada
+ function dizernome(){ 
+     console.log('otavio');
+ }
+
+ dizernome();
+//faz um cálculo ou operação e retorna algo
+ function multiplicarpordois(valor){
+     return valor* 3;
+
+ }
+ //console.log(multiplicarpordois(10));
+
+ var resultado = multiplicarpordois(5);
+ 
+ console.log(resultado);
