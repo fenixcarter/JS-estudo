2535bc3bc6de50efb8e6a0c8b576156201170df2
Otavio Renan <fenixkc@hotmail.com>
2021-10-30 14:38:46 -0300

    adicionada função de atirar

--- function.js
+++ function.js
@@ -22,3 +22,18 @@ function ativarDesativarLuz(ligado,desligado,semEnergia){
 console.log(ativar);
 ativarDesativarLuz("ligar luz", "desligar luz", "Faltou Energia");
 console.log(standBy);
+
+
+
+var atirar = "acertar";
+function dar1HS(tironacabeca, acertarpeito, correr, susto){
+   atirar = tironacabeca;
+   errar = acertarpeito;
+   quaseacertar = correr;
+   descarregou = susto;
+   
+}
+
+console.log(atirar);
+dar1HS("mata", " foge", "correr", "sem munição");
+console.log(descarregou,atirar);
