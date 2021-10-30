56c4887d4eb981e762906394c8f0e53d3e102e27
Otavio Renan <fenixkc@hotmail.com>
2021-10-30 14:11:06 -0300

    função basica

--- function.js
+++ function.js
@@ -9,3 +9,16 @@ console.log(direcao);
 mudarDirecao("primeiro esquerda","depois direta");
 console.log(direcao);
 
+
+let ativar = "ligarr";
+function ativarDesativarLuz(ligado,desligado,semEnergia){
+    ativado = ligado;
+    desativado = desligado;
+    standBy = semEnergia;
+    
+
+}
+
+console.log(ativar);
+ativarDesativarLuz("ligar luz", "desligar luz", "Faltou Energia");
+console.log(standBy);
