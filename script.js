db6d7e502e910aa6da8541417a787e0034b3527e
Otavio Renan <fenixkc@hotmail.com>
2021-10-30 10:53:38 -0300

    aprendendo função let and const

---
+++ script.js
@@ -0,0 +1,24 @@
+//let idade = 5;
+//console.log(idade);
+//let altura = 180
+//console.log(altura);
+//camelCase
+//let = variavel
+// variaveis nao podem começar com maiusculo
+// = definindo atribuiçao 
+//; finalizaçao
+
+let nome = 'otavio';
+console.log (nome);
+
+let idade = '25'
+console.log (idade);
+
+let estadoCivil = 'solteiro';
+console.log (estadoCivil);
+
+
+//const usado quando valor nao precisa ser redefinido, let quando precisar alterar valores
+
+const valoCarro = 30;
+console.log (valoCarro);
