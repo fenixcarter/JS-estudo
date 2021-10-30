5a6415ef8f369a5c9d28072a06de17094b52de3b
Otavio Renan <fenixkc@hotmail.com>
2021-10-30 15:05:04 -0300

    operadores

---
+++ operadores.js
@@ -0,0 +1,27 @@
+//Operadores aritimeticos(matematicos)
+//operadores atriuição
+//operadores de comparação
+//operadores lógicos
+// operadores bitwise
+
+
+
+//operadores aritimeticos
+
+ let salario = 100;
+
+// + , - , * , / , **
+
+//operadres basicos
+//console.log(salario + salario);
+//console.log(salario - salario);
+//console.log(salario * salario);
+//console.log(salario / salario);
+//console.log(5 ** 5);
+
+// ++ --
+
+let idade = 18;
+
+console.log(--idade);
+console.log(idade);
