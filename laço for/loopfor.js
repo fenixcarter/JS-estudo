7c3b1968c71831bfb732a8caaf31865766fc165c
Otavio Renan <fenixkc@hotmail.com>
2021-10-31 13:04:12 -0300

    estudo loopfor

---
+++ laço for/loopfor.js
@@ -0,0 +1,16 @@
+//1. For
+//2. WHile
+//3. do..while
+//4. For..in
+//5. For..of
+
+//1.For
+
+for(let i = 0; i < 5; i++) {
+    if(i % 2 !== 0){
+        console.log(i);
+    }
+}
+
+//while loop
+let i = 5;
