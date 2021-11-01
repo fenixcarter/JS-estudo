c0547af277f565c7c4f3c4b679308b15a35a7105
Otavio Renan <fenixkc@hotmail.com>
2021-11-01 20:35:24 -0300

    script2.js modificado

--- script2.js
+++
@@ -1,7 +0,0 @@
-let nome = 'renan'; //string literal (varias letras em uma palavra, armazenado em uma variavel)
-let idade = 28; //number literall
-let estaAprovado = true; //boolean (ajuda a construir regras no sistema,true and false)
-let sobrenome = undefined; //undefined
-let corSelecionada = null //usado para redifinir um valor.
-
-
